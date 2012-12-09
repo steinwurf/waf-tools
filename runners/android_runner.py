fb2a7bfb4bf9290378755357e87888a07d454bbb
Jeppe Pihl <jpihl08@gmail.com>
2012-12-09 21:00:36 +0100

    Android runner is now running in tmp directory (to enable creation of files etc.)

--- runners/android_runner.py
+++ runners/android_runner.py
@@ -21,21 +21,23 @@ class AndroidRunner(BasicRunner):
 
         result = (self.format_command(self.inputs[0]), combined_return_code,
                   combined_stdout, combined_stderr)
-        
+
         super(AndroidRunner, self).save_result(result)
-    
+
     def run(self):
 
         bld = self.generator.bld
-        
+
         adb = bld.env['ADB']
 
         results = []
-        
+
+        dest_dir = '/data/local/tmp/'
+
         def run_cmd(cmd):
 
             Logs.debug("wr: running %r", cmd)
-            
+
             proc = Utils.subprocess.Popen(
                 cmd,
                 stderr=Utils.subprocess.PIPE,
@@ -47,7 +49,7 @@ class AndroidRunner(BasicRunner):
 
             return result
 
-        dest_bin = '/data/local/tmp/' + str(self.inputs[0])
+        dest_bin = dest_dir + str(self.inputs[0])
 
         device_id = None
         if bld.has_tool_option('device_id'):
@@ -56,13 +58,13 @@ class AndroidRunner(BasicRunner):
 
         # Run the two adb commands
         adb_push = []
-        
+
         if device_id:
             adb_push += [adb, '-s', device_id, 'push']
         else:
             adb_push += [adb, 'push']
 
-        adb_push += [str(self.inputs[0].abspath()), dest_bin] 
+        adb_push += [str(self.inputs[0].abspath()), dest_bin]
 
         result = run_cmd(adb_push)
         results.append(result)
@@ -70,7 +72,7 @@ class AndroidRunner(BasicRunner):
         if result['return_code'] != 0:
             self.save_result(results)
             return
-        
+
         cmd = self.format_command(dest_bin)
 
         # Echo the exit code after the shell command
@@ -81,7 +83,8 @@ class AndroidRunner(BasicRunner):
         else:
             adb_shell += [adb, 'shell']
 
-        adb_shell += [cmd + ';echo shellexit:$?']
+        # We have to cd to the dir
+        adb_shell += ["cd %s;%s;echo shellexit:$?" % (dest_dir, cmd)]
 
         result = run_cmd(adb_shell)
         results.append(result)
