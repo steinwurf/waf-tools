83668be27b9974adeb899f534d984e9977a1e8c1
Morten V. Pedersen <morten@mortenvp.dk>
2013-01-24 10:17:07 +0100

    Adding option to preserve folder structure via the waf option relative_trick

--- install_path/wurf_install_path.py
+++ install_path/wurf_install_path.py
@@ -27,21 +27,46 @@ def configure(conf):
     if conf.is_toplevel():
         conf.load('wurf_dependency_bundle')
         conf.load('wurf_tools')
-        conf.load_external_tool('install_path', 'wurf_install_path')    
+        conf.load_external_tool('install_path', 'wurf_install_path')
 
 
 The install path may now be updated by passing the install_path options. E.g.
 ./waf --options=install_path=~/my_binaries
 
+If you also pass the install_relative option waf will preserve the folder
+structure when installing e.g.:
+    ./waf --options=install_path=~/my_binaries,install_relative
+
 """
 
 import os
-from waflib.TaskGen import feature, before_method
+from waflib.TaskGen import feature, before_method, after_method
 
 @feature('cxxprogram')
 @before_method('apply_link')
 def update_install_path(self):
+    """
+    Updates the install_path variable of the task generator before
+    executing the apply_link method. This will override the install_path
+    otherwise used.
+    """
 
     if self.bld.has_tool_option('install_path'):
         install_path = self.bld.get_tool_option('install_path')
         self.install_path = os.path.abspath(os.path.expanduser(install_path))
+
+
+@feature('cxxprogram')
+@after_method('apply_link')
+def change_relative_path_option(self):
+    """
+    If an install_task exists this will update the relative_trick
+    options, which will make waf preserve the folder structure
+    """
+
+    install_relative = self.bld.has_tool_option('install_relative')
+
+    if self.install_task:
+        self.install_task.relative_trick = install_relative
+
+
