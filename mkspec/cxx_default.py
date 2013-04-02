845c1782ee87d7d1fc31e78b6aacaed33fcc1e31
Peter Vingelmann <peter@steinwurf.com>
2013-04-02 22:25:14 +0200

    adding wurf_project_generator.py + msvc debug builds

--- mkspec/cxx_default.py
+++ mkspec/cxx_default.py
@@ -291,10 +291,21 @@ def mkspec_set_android_common(conf):
 
 
 @conf
-def mkspec_set_msvc_cxxflags(conf):
+def mkspec_set_msvc_flags(conf):
+
+    # Set _CRT_SECURE_NO_WARNINGS and _SCL_SECURE_NO_WARNINGS to suppress
+    # deprecation warnings for strcpy, sprintf, etc.
+    if conf.has_tool_option('cxx_debug'):
+        # Produce full-symbolic debugging information in a .pdb file
+        # Use the multithread, debug version of the run-time library
+        conf.env['CXXFLAGS'] += ['/Zi', '/MTd', '/D_SCL_SECURE_NO_WARNINGS']
+        conf.env['LINKFLAGS'] += ['/DEBUG']
+    else:
+        # Use the multithread, release version of the run-time library
+        conf.env['CXXFLAGS'] += ['/MT', '/D_CRT_SECURE_NO_WARNINGS']
+
     # Set _WIN32_WINNT=0x0501 (i.e. Windows XP target)
     # to suppress warnings in boost asio
-    # Set _CRT_SECURE_NO_WARNINGS to suppress
-    # deprecation warnings for strcpy, sprintf, etc.
-    conf.env['CXXFLAGS'] += ['/O2', '/Ob2', '/W3', '/MT', '/EHs',
-        '/D_WIN32_WINNT=0x0501', '/D_CRT_SECURE_NO_WARNINGS']
+    conf.env['CXXFLAGS'] += ['/O2', '/Ob2', '/W3', '/EHs',
+        '/D_WIN32_WINNT=0x0501']
+
---
+++ project_gen/wurf_project_generator.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+"""
+This tool is a container that bundles various project generators.
+
+The available generators:
+
+    wurf_msvs:  Generates Visual Studio 2008, 2010 or 2012 solutions
+                Exported Waf commands:
+                    msvs2008
+                    msvs2010
+                    msvs2012
+
+
+To use this external tool, simply load it in configure:
+
+def configure(conf):
+
+    if conf.is_toplevel():
+        conf.load_external_tool('project_gen', 'wurf_project_generator')
+
+Then you can invoke the tool together with configure:
+
+$ python waf configure msvs2012
+"""
+
+import wurf_msvs
