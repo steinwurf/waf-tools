63d10f956161f66bb0da644b78d2c7688f2d38cd
Peter Vingelmann <peter@steinwurf.com>
2013-06-07 19:48:26 +0200

    check only one compiler if CXX variable is set

--- mkspec/cxx_default.py
+++ mkspec/cxx_default.py
@@ -52,6 +52,22 @@ Detect and setup the default compiler for the platform
 """
 def configure(conf):
 
+    # If the user-defined CXX variable is set
+    # then use that compiler as the first option
+    if 'CXX' in os.environ:
+        compiler = os.environ['CXX']
+        conf.start_msg('Checking C++ compiler %r' % compiler)
+        load_compiler(conf, compiler)
+        if conf.env['CXX']:
+            conf.end_msg(conf.env.get_flat('CXX'))
+            conf.env['COMPILER_CXX'] = compiler
+            return # Compiler configured successfully
+        else:
+            conf.end_msg(False)
+            conf.fatal('Could not configure a C++ compiler!')
+
+    # Otherwise we try to find a compiler on the current host
+    # based on the following compiler list
     cxx_compilers = \
     {
         'win32':  ['msvc', 'g++'],
@@ -61,21 +77,14 @@ def configure(conf):
         'default': ['g++']
     }
 
-    # Here we try to find a compiler on the current host
-    # based on the compiler list above
     sys_platform = Utils.unversioned_sys_platform()
     platform = 'default'
     # Check if we have a specific list for the current system
     if sys_platform in cxx_compilers:
         platform = sys_platform
 
-    possible_compiler_list = []
-    # If the user-defined CXX variable is set
-    # then use that compiler as the first option
-    if 'CXX' in os.environ:
-        possible_compiler_list += [os.environ['CXX']]
-    # Otherwise use the default compilers for the current platform
-    possible_compiler_list += cxx_compilers[platform]
+    # The list of the compilers to be checked
+    possible_compiler_list = cxx_compilers[platform]
 
     for compiler in possible_compiler_list:
         conf.env.stash()
