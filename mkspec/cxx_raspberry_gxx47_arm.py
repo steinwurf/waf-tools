a6042180f70be916351c59c7e26ba9291b04cdb4
Peter Vingelmann <petya@master.com>
2013-08-14 19:42:58 +0200

    correct mkspec/cxx_raspberry_gxx47_arm.py

--- mkspec/cxx_raspberry_gxx47_arm.py
+++ mkspec/cxx_raspberry_gxx47_arm.py
@@ -2,9 +2,8 @@
 # encoding: utf-8
 
 """
-Detect and setup the g++ 4.6 cross-compiler for 32 bit Linux
+Detect and setup the g++ 4.7 cross-compiler for Raspberry Pi (Linux)
 """
 def configure(conf):
     conf.load_external_tool('mkspec_common', 'gxx_common')
-    conf.mkspec_gxx_configure(4, 6, 'crosslinux-gxx46-x86')
-    conf.env['CXXFLAGS'] += ['-m32']
+    conf.mkspec_gxx_configure(4, 7, 'raspberry-gxx47-arm')
