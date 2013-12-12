9064896b2dbb44fead7f6bd9d7a7a44c80a9cf9d
Peter Vingelmann <petya@master.com>
2013-12-12 02:51:07 +0100

    support for loading kernel objects in basic_runner

--- runners/wurf_runner.py
+++ runners/wurf_runner.py
@@ -113,6 +113,16 @@ def make_run(taskgen, run_type):
         test_files = getattr(taskgen, 'test_files', [])
         task.test_inputs = taskgen.to_nodes(test_files)
 
+        # Check if the executable requires any kernel modules
+        kernel_modules = getattr(taskgen, 'kernel_modules', [])
+        if kernel_modules:
+            task.kernel_objects = []
+            for module in kernel_modules:
+                # Find the task that builds the module
+                module_task = taskgen.bld.get_tgen_by_name(module).tasks[0]
+                # Get the first target of the task (i.e. the kernel object)
+                task.kernel_objects.append(module_task.outputs[0])
+
     # We are creating a new task which should run an executable after
     # a build finishes. Here we add two functions to the BuildContext
     # which prints a summary and ensures that the build fails if the
