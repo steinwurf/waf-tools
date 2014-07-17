4205dbc0229659aa3f290db3327e90a9fabcf453
Jeppe Pihl <jpihl08@gmail.com>
2014-07-17 19:48:46 +0200

    add architecture cxx mkspec

--- mkspec/cxx_default.py
+++ mkspec/cxx_default.py
@@ -6,21 +6,31 @@ import os
 from waflib import Errors
 from waflib import Logs
 from waflib import Utils
+from waflib.Configure import conf
 
 import clang_mkspecs
 import gxx_mkspecs
 import msvc_mkspecs
 
 
-def load_compiler(conf, compiler):
+def load_compiler(conf, compiler, arch):
+    if ('clang' in compiler or 'g++' in compiler) and arch:
+        if arch == 'x86':
+            conf.mkspec_add_common_flag('-m32')
+        elif arch == 'x64':
+            conf.mkspec_add_common_flag('-m64')
 
     # Note clang goes first otherwise 'g++' will be in 'clang(g++)'
     if 'clang' in compiler:
         conf.mkspec_clang_configure(3, 4, minimum=True)
-
     elif 'g++' in compiler:
         conf.mkspec_gxx_configure(4, 7, minimum=True)
     elif 'msvc' in compiler or 'CL.exe' in compiler or 'cl.exe' in compiler:
+        if arch == 'x86':
+            conf.env.MSVC_TARGETS = ['x86']
+        elif arch == 'x64':
+            conf.env.MSVC_TARGETS = ['x86_amd64']
+
         conf.load('msvc')
         # Note: the waf msvc tool also load msvc as a C compiler
         conf.mkspec_check_minimum_msvc_version(12.0)
@@ -29,7 +39,8 @@ def load_compiler(conf, compiler):
         raise Errors.WafError('Unknown compiler: %s' % compiler)
 
 
-def configure(conf):
+@conf
+def cxx_default(conf, arch=None):
     """
     Detect and setup the default compiler for the platform
     """
@@ -38,7 +49,7 @@ def configure(conf):
     if 'CXX' in os.environ:
         compiler = os.environ['CXX']
         conf.start_msg('Checking C++ compiler %r' % compiler)
-        load_compiler(conf, compiler)
+        load_compiler(conf, compiler, arch)
         if conf.env['CXX']:
             conf.end_msg(conf.env.get_flat('CXX'))
             conf.env['COMPILER_CXX'] = compiler
@@ -49,14 +60,13 @@ def configure(conf):
 
     # Otherwise we try to find a compiler on the current host
     # based on the following compiler list
-    cxx_compilers = \
-        {
-            'win32':  ['msvc', 'g++'],
-            'linux':  ['g++', 'clang++'],
-            'darwin': ['clang++', 'g++'],
-            'cygwin': ['g++'],
-            'default': ['g++']
-        }
+    cxx_compilers = {
+        'win32':  ['msvc', 'g++'],
+        'linux':  ['g++', 'clang++'],
+        'darwin': ['clang++', 'g++'],
+        'cygwin': ['g++'],
+        'default': ['g++']
+    }
 
     sys_platform = Utils.unversioned_sys_platform()
     platform = 'default'
@@ -71,7 +81,7 @@ def configure(conf):
         conf.env.stash()
         conf.start_msg('Checking for %r (C++ compiler)' % compiler)
         try:
-            load_compiler(conf, compiler)
+            load_compiler(conf, compiler, arch)
         except conf.errors.ConfigurationError as e:
             conf.env.revert()
             conf.end_msg(e, color='YELLOW')
@@ -84,3 +94,13 @@ def configure(conf):
             conf.end_msg(False)
     else:
         conf.fatal('Could not configure a C++ compiler!')
+
+
+@conf
+def cxx_default_x86(conf):
+    cxx_default(conf, 'x86')
+
+
+@conf
+def cxx_default_x64(conf):
+    cxx_default(conf, 'x64')
