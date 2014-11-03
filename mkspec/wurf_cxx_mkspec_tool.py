1bc0faa9aa6a45e8e93ff1d6463308670b453ae3
Jeppe Pihl <jpihl08@gmail.com>
2014-11-03 11:10:27 +0100

    removed debug print

--- mkspec/wurf_cxx_mkspec_tool.py
+++ mkspec/wurf_cxx_mkspec_tool.py
@@ -19,7 +19,6 @@ def get_mkspec_platform(conf):
     # If the MKSPEC_PLATFORM is not set, we auto detect it.
     if not conf.env['MKSPEC_PLATFORM']:
         platform = Utils.unversioned_sys_platform()
-        print platform
         if platform == 'win32':
             platform = 'windows'
         elif platform == 'darwin':
