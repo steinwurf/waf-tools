0bc919aa8aea5acb3ee3485904cbf5fd0238d46d
Peter Vingelmann <petya@master.com>
2015-05-01 01:52:14 +0200

    Do not start usbmuxd in ios_runner (the process will run as a system service instead)

--- runners/ios_runner.py
+++ runners/ios_runner.py
@@ -9,46 +9,13 @@ from ssh_runner import SSHRunner
 
 class IOSRunner(SSHRunner):
 
-    def save_result(self, results, ssh_cmd):
-
-        # Kill the usbmux process
-        if self.usbmux_proc:
-            self.usbmux_proc.kill()
-
-        super(IOSRunner, self).save_result(results, ssh_cmd)
-
     def run(self):
 
         bld = self.generator.bld
-        self.usbmux_proc = None
 
         dest_dir = '/private/var/mobile/tmp'
         localport = '22222'
         ssh_target = 'mobile@localhost'
-
-        def start_proc(cmd):
-
-            Logs.debug("wr: starting %r", cmd)
-
-            proc = Utils.subprocess.Popen(
-                cmd,
-                stderr=Utils.subprocess.PIPE,
-                stdout=Utils.subprocess.PIPE)
-            # Wait for 3 seconds so that the process can start properly
-            # If the device was idle for a long time, then the connection
-            # might not start for the first try
-            time.sleep(3.0)
-
-            return proc
-
-        usbmux_dir = bld.get_tool_option('usbmux_dir')
-
-        usbmux = os.path.join(usbmux_dir, 'tcprelay.py')
-        # Use usbmux in threaded mode to handle multiple connections at once
-        usbmux_cmd = [usbmux, '--threaded', '22:{}'.format(localport)]
-
-        # Start the usbmux daemon to forward 'localport' to port 22 on USB
-        self.usbmux_proc = start_proc(usbmux_cmd)
 
         ssh_options = []
         if bld.has_tool_option('ssh_options'):
