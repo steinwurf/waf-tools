f5bce3c357310bd8db7f4c15fddc253a734f36f8
Peter Vingelmann <petya@master.com>
2015-09-29 19:56:39 +0200

    Add cxx_apple_llvm70_x64

--- mkspec/clang_mkspecs.py
+++ mkspec/clang_mkspecs.py
@@ -121,6 +121,19 @@ def cxx_apple_llvm61_x64(conf):
 
 
 @conf
+def cxx_apple_llvm70_x64(conf):
+    """
+    Detect and setup the 64-bit Apple LLVM 7.0 compiler
+    """
+    if conf.is_mkspec_platform('mac'):
+        conf.mkspec_clang_configure(7, 0)
+        conf.mkspec_add_common_flag('-m64')
+    else:
+        conf.fatal("This mkspec is not supported on {0}.".format(
+            conf.get_mkspec_platform()))
+
+
+@conf
 def cxx_clang30_x64(conf):
     """
     Detect and setup the clang 3.0 compiler for 64 bit
