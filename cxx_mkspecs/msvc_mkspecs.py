9d6129b49907c09784cb0d4d2d3dcf7ca2c29764
Peter Vingelmann <petya@master.com>
2018-08-31 20:38:26 +0200

    Add mkspecs for VS2017

--- cxx_mkspecs/msvc_mkspecs.py
+++ cxx_mkspecs/msvc_mkspecs.py
@@ -56,3 +56,29 @@ def cxx_msvc14_x86(conf):
     else:
         conf.fatal("This mkspec is not supported on {0}.".format(
             conf.get_mkspec_platform()))
+
+
+@conf
+def cxx_msvc15_x64(conf):
+    """
+    Detect and setup the Microsoft Visual C++ 2017 compiler for 64-bit
+    """
+    if conf.is_mkspec_platform('windows'):
+        conf.env.MSVC_TARGETS = ['x64']
+        conf.mkspec_msvc_configure('15.0')
+    else:
+        conf.fatal("This mkspec is not supported on {0}.".format(
+            conf.get_mkspec_platform()))
+
+
+@conf
+def cxx_msvc15_x86(conf):
+    """
+    Detect and setup the Microsoft Visual C++ 2017 compiler for 32-bit
+    """
+    if conf.is_mkspec_platform('windows'):
+        conf.env.MSVC_TARGETS = ['x86']
+        conf.mkspec_msvc_configure('15.0')
+    else:
+        conf.fatal("This mkspec is not supported on {0}.".format(
+            conf.get_mkspec_platform()))
