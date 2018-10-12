9aa660d259ebda9dcbfe62648cfb552badddc62c
Peter Vingelmann <petya@master.com>
2018-10-12 21:55:50 +0200

    Add mkspec for Apple LLVM 10.0

--- cxx_mkspecs/clang_mkspecs.py
+++ cxx_mkspecs/clang_mkspecs.py
@@ -219,6 +219,19 @@ def cxx_apple_llvm91_x64(conf):
 
 
 @conf
+def cxx_apple_llvm100_x64(conf):
+    """
+    Detect and setup the 64-bit Apple LLVM 10.0 compiler
+    """
+    if conf.is_mkspec_platform('mac'):
+        conf.mkspec_clang_configure(10, 0)
+        conf.mkspec_add_common_flag('-m64')
+    else:
+        conf.fatal("This mkspec is not supported on {0}.".format(
+            conf.get_mkspec_platform()))
+
+
+@conf
 def cxx_clang36_x64(conf):
     """
     Detect and setup the clang 3.6 compiler for 64 bit
