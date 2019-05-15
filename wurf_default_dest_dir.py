77ee4683859851e887b55c1d9dadf7c69aa2f975
Morten V. Pedersen <morten@mortenvp.com>
2019-05-15 16:51:13 +0200

    working on it

--- wurf_default_dest_dir.py
+++ wurf_default_dest_dir.py
@@ -15,17 +15,16 @@ def options(opt):
     group = opt.option_groups['Configuration options']
     group.remove_option('--destdir')
 
-    appname = getattr(waflib.Context.g_module, 'APPNAME')
-    version = getattr(waflib.Context.g_module, 'VERSION')
-
-    default_destdir = os.path.join(
-        opt.srcnode.abspath(), appname + '_' + version)
+    default_destdir = opt.srcnode.abspath()
 
     group.add_option('--destdir',
                      help='installation root [default: %r]' % default_destdir,
                      default=default_destdir, dest='destdir')
 
+    appname = getattr(waflib.Context.g_module, 'APPNAME')
+    version = getattr(waflib.Context.g_module, 'VERSION')
+
     group.remove_option('--prefix')
-    default_prefix = ''
+    default_prefix = os.path.join(os.sep, appname + '_' + version)
     group.add_option('--prefix', dest='prefix', default=default_prefix,
                      help='installation prefix [default: %r]' % default_prefix)
